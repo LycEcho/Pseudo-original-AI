c1a650fa5e6fede37f6f08940c1670121a756ca4
lycecho <488043287@qq.com>
2022-03-22 20:17:38 +0800

    增加 英文吧

---
+++ README.php
@@ -0,0 +1,50 @@
+# WYC-API
+
+Intelligent ai article writing pseudogenic interface API Updated: 2022/4/1
+
+### Interface introduction
+
+Support 6 pseudogenic algorithm
+
+Not a single keyword replace, but the statement combination is replaced, and the data is constantly updated.
+
+Support train head plug-in
+
+### Online experience effect
+Http://cj.lycecho.com/api_form.php?key=test123
+
+### Case
+## 文文
+# 红尘, branches, petals fluttering, my soul fly outside the world, doing the dance in the year. In order to meet you, drifting is not far away, just for this summer. South China testimony, high temperature, water does not give up. I like this kind of feelings, enthusiasm, test the endurance of the body, and test the ability to love. I want to tell you, your blood has flown through my heart, I want to tell the wind, it's soft, I also appreciate, I want to tell the summer, I will return to the Northeast to take the summer, linger with you. I drifting the south, leaving my hometown, leaving you. Just like petals, just like butterfly elegance, I really want to bend down the waist and pick up this aftertaste, put it in the palm. It can still taste this fragrance, so many memories, there are many thoughts, there is love. The enthusiasm of South China, always let those footprints become memories. You can meet in the reality, is it possible? Can you come to me?
+
+## 伪 原 创
+# 红尘, branches, petals floating, my soul fly out of the world, and dance with words in the flow. In order to meet you, I won't be a thousand miles, only for this summer romance. South China proves that the temperature is high, and the water will not be discarded. I like this feeling, full of passion, testing the body's endurance and love. I want to tell you, your blood has flowed through my heart. I want to tell the wind, I have experienced its softness. I want to tell you, in the summer, I want to return to the northeast with you to spend the summer. I wander in the south, leaving my hometown, left you. I like the petals to fall, I like the butterfly elegant, I really want to bend this fragrant, put it in the hands. You can still taste this scent, let a lot of memories, there are many thoughts, and love. The enthusiasm of South China Sunshine always makes those footprints a memories. I see you in reality, can I achieve it? Can you come to me?
+
+
+### interface address
+
+| URL | POST Transfer Parameters |
+| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ --------- |
+| http://cj.lyceno.com/api.php?json=0&v=1&key= token | {wenzhang: 'Article content'} |
+| http://cj.lyceno.com/api.php?json=0&v=2&key= Apply token | {Wenzhang: 'Article content'} |
+| Test token: Test123 | Test token daily free pseudo original query 10W times | |
+
+
+### buy online
+http://pay.lyceno.com
+
+### Charge mode
+
+Since the server is expensive every month, the open interface is divided into payment + free mode, both models provide HTML + JSON version interface
+
+Both interfaces do not include advertising, the paid version can have its own token number request, the test version shares sharing the number of times a day, no
+
+| Function Description | Paid Version | Free Version |
+| -------------------------------------- |
+| Price | 100 yuan / month | Free |
+| JSON interface | √ | √ |
+| QPS concurrency | Not limited | 1 concurrency |
+| Japanese requests | Unlimited | Sharing 10W |
+| Ip White List | √ | × |
+| Technical Support (Simple) | √ | × |
+QQ consultation (599858092) | √ | √ |
